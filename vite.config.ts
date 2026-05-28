ade64781b10a8710dfde006a1322c1b20d1af468
cosformula <cosformula@gmail.com>
2026-05-28 15:37:08 +0800

    fix: disable pwa navigation fallback

--- vite.config.ts
+++ vite.config.ts
@@ -3,51 +3,57 @@ import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			buildBase: '/',
-			scope: '/',
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'MDXport',
-				short_name: 'MDXport',
-				description: 'Markdown to PDF Professional Export Tool',
-				start_url: '/',
-				scope: '/',
-				theme_color: '#ffffff',
-				icons: [
-					{
-						src: 'favicon-16x16.png',
-						sizes: '16x16',
-						type: 'image/png'
-					},
-					{
-						src: 'favicon-32x32.png',
-						sizes: '32x32',
-						type: 'image/png'
-					},
-					{
-						src: 'logo.png',
-						sizes: '183x100',
-						type: 'image/png'
-					},
-					{
-						src: 'apple-touch-icon.png',
-						sizes: '180x180',
-						type: 'image/png'
-					},
-					{
-						src: 'square.png',
-						sizes: '240x240',
-						type: 'image/png',
-						purpose: 'any maskable'
-					}
-				]
-			}
-		})
-	],
-	worker: {
-		format: 'es'
-	}
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      buildBase: '/',
+      scope: '/',
+      registerType: 'autoUpdate',
+      kit: {
+        trailingSlash: 'always'
+      },
+      workbox: {
+        navigateFallback: null
+      },
+      manifest: {
+        name: 'MDXport',
+        short_name: 'MDXport',
+        description: 'Markdown to PDF Professional Export Tool',
+        start_url: '/',
+        scope: '/',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'logo.png',
+            sizes: '183x100',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: 'square.png',
+            sizes: '240x240',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  worker: {
+    format: 'es'
+  }
 });
